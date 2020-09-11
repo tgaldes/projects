794dcda81e63e214579b937231c749f4736e6587
Tyler <tgaldes@gmail.com>
2020-09-10 22:52:29 -0400

    V1 done- can post to non buy and sell groups

--- cfld/pyautogui/inputs.py
+++ cfld/pyautogui/inputs.py
@@ -1,26 +1,50 @@
 from collections import namedtuple
-school_data = [(
-        # USC
-         'https://docs.google.com/document/d/1wOmbPiLod6B-WPjF4ygP6JbjqGP8Em4VXPMk0yekX_8/edit',
-         '/home/tgaldes/Dropbox/Fraternity PM/Pictures/2715 Portland USC/selections',
-         [
-         #('https://www.facebook.com/groups/1621705121404017/', 2),
-         #('https://www.facebook.com/groups/695435553986189/', 2), 
-         #('https://www.facebook.com/groups/344453725628321/', 2),
-         #('https://www.facebook.com/groups/344314769393545', 2),
-         ('https://www.facebook.com/groups/212185346023652/', 1),
-         ('https://www.facebook.com/groups/143384839561246/', 3),
-         ('https://www.facebook.com/groups/161010671290969/', 1)],
-         (1194, 1419), # coords to the 'post' button when we c/p the blurb into the text box
-         (1113, 282)  # coords to the area that we check for post pending
-        )]
+school_data = {'USC' :(
+             'https://docs.google.com/document/d/1wOmbPiLod6B-WPjF4ygP6JbjqGP8Em4VXPMk0yekX_8',
+             '/home/tgaldes/Dropbox/Fraternity PM/Pictures/2715 Portland USC/selections',
+             [
+                 ('https://www.facebook.com/groups/1621705121404017/', 2),
+                 ('https://www.facebook.com/groups/695435553986189/', 2), 
+                 ('https://www.facebook.com/groups/344453725628321/', 2),
+                 ('https://www.facebook.com/groups/344314769393545', 2),
+                 ('https://www.facebook.com/groups/212185346023652/', 1),
+                 ('https://www.facebook.com/groups/143384839561246/', 3),
+                 ('https://www.facebook.com/groups/161010671290969/', 1)
+             ],
+             (1194, 1419), # coords to the 'post' button when we c/p the blurb into the text box
+             (1113, 282)  # coords to the area that we check for post pending
+        ),
+        'SJSU' :
+        (
+            'https://docs.google.com/document/d/1jxyqgdu0EXNK73gg5kcapxTcLc0XilB1qrkAbe31BtU',
+            '/home/tgaldes/Dropbox/Fraternity PM/Pictures/SJSU TX/facebook',
+            [
+                ('https://www.facebook.com/groups/948845475189394/', 1),
+                ('https://www.facebook.com/groups/SJSUhousing/', 2),
+                ('https://www.facebook.com/groups/574981909329531/', 2)
+            ],
+             (1194, 1419), # coords to the 'post' button when we c/p the blurb into the text box
+             (1113, 282)  # coords to the area that we check for post pending
+        ),
+        'UMN' :
+        (
+            'https://docs.google.com/document/d/17Kx_8SGCm3QA-ArL8o7GU99cOyqjLJX02lJogHlAvKg',
+            '/home/tgaldes/Dropbox/Fraternity PM/Pictures/Minnesota Kappa Sigma/Facebook',
+            [
+                ('https://www.facebook.com/groups/439773589790851/', 3),
+                ('https://www.facebook.com/groups/153997765269550/', 2)
+            ],
+             (1194, 1419), # coords to the 'post' button when we c/p the blurb into the text box
+             (1113, 282)  # coords to the area that we check for post pending
+         )
+        }
 
-GroupData = namedtuple('GroupData', ['google_drive_link', 'folder_path', 'group_link', 'group_name_length', 'post_button_coords', 'post_pending_coords'])
+GroupData = namedtuple('GroupData', ['google_drive_link', 'folder_path', 'group_link', 'group_name_length', 'post_button_coords', 'post_pending_coords', 'short_name'])
 
 all_groups = []
-for school in school_data:
-    for group in school[2]:
-        data = GroupData(school[0], school[1], group[0], group[1], school[3], school[4])
+for short_name in school_data:
+    for group in school_data[short_name][2]:
+        school = school_data[short_name]
+        data = GroupData(school[0], school[1], group[0], group[1], school[3], school[4], short_name)
         all_groups.append(data)
-        
 
--- cfld/pyautogui/main.py
+++ cfld/pyautogui/main.py
@@ -21,14 +21,15 @@ all_coords = {
             'chrome' : (168, 170),
             'close_chrome' : (91, 46),
             'url_bar' : (562, 78),
-            'one_line_group_photo_video' : (1063, 801),
-            'two_line_group_photo_video' : (1063, 831),
+            'one_line_group_photo_video' : (1066, 786),
+            'two_line_group_photo_video' : (1066, 820),
             'file_upload_path' : (1202, 250),
             'write_something' : (1144, 590),
-            'three_line_group_photo_video' : (1066, 863),
+            'three_line_group_photo_video' : (1066, 853),
             'tab_one' : (264, 60),
             'tab_two' : (540, 60),
-            'google_drive_ready' : (949, 286)
+            'google_drive_ready' : (949, 286),
+            'open_files_bar' : (800, 225)
         }
     }
 mkey = 'm' # TODO
@@ -43,6 +44,7 @@ facebook_post_photo_video_color_bk = (116, 173, 98) # green in the button
 facebook_ready_to_post_color = (72, 123, 237) # blue
 drive_ready_color = (255, 255, 255)
 chrome_url_bar = (255, 255, 255)
+open_file = (71, 70, 64)
 
 def mouse_click(item, sleep_secs=0):
     pag.click(*coords[item])
@@ -77,46 +79,51 @@ def wait_for_screen(pixel, match, item, x=0, y=0):
         x, y = coords[item]
     count = 0
     while True:
-        if wait_loop(pixel, match, x, y, count):
-            return
+        if match == pag.pixelMatchesColor(x, y, pixel):
+            return True
+        screen = pag.screenshot()
+        print('pixel {} coords {} matching {} actual {}'.format((x, y), pixel, match, screen.getpixel((x, y))))
+        if count > 120:
+            print('timing out after two minutes, still loc {} pixel to match {} matching {}'.format((x, y), pixel, match))
+            return False
         sleep(1)
         count += 1
 
-def wait_for_screen_and_scroll(pixels, match, item, x=0, y=0):
-    if x == 0 and y == 0:
-        x, y = coords[item]
+def clean_up():
+    press('ctrl', 'w')
+    press('ctrl', 'w')
+    mouse_click('close_chrome')
+
+def open_photo_upload(nt):
     count = 0
     while True:
+        screen = pag.screenshot()
+        move_to(group_name_mapping[nt.group_name_length]) # move cursor back to main page
         pag.scroll(1000)
-        for pixel in pixels:
-            if wait_loop(pixel, match, x, y, count):
-                return
+        mouse_click(group_name_mapping[nt.group_name_length]) # click the 'photo/video button'
         sleep(1)
+        if pag.pixelMatchesColor(*coords['open_files_bar'], open_file):
+            return True
         count += 1
+        print('pixel {} coords {} matching {} actual {}'.format(coords['open_files_bar'], open_file, True, screen.getpixel(coords['open_files_bar'])))
+        if count > 120:
+            return False
 
-def wait_loop(pixel, match, x, y, count):
-    if match == pag.pixelMatchesColor(x, y, pixel):
-        return True
-    screen = pag.screenshot()
-    print('pixel {} coords {} matching {} actual {}'.format((x, y), pixel, match, screen.getpixel((x, y))))
-    if count > 120:
-        print('timing out after two minutes, still loc {} pixel to match {} matching {}'.format((x, y), pixel, match))
-        exit(1)
-    return False
+
 
 #def run_group(google_doc_link, folder_path, group_tup, post_coords):
 def run_group(nt): #google_doc_link, folder_path, group_tup, post_coords):
     mouse_click('menu')
     keyboard('Chrome')
     mouse_click('chrome')
-    wait_for_screen(chrome_url_bar, True, 'url_bar')
+    if not wait_for_screen(chrome_url_bar, True, 'url_bar'):
+        clean_up()
     open_tab(nt.group_link)
     press('ctrl', 't')
     open_tab(nt.google_drive_link)
     press('ctrl', 'pageup') # back to facebook tab
-    move_to(group_name_mapping[nt.group_name_length]) # move cursor back to main page
-    wait_for_screen_and_scroll([facebook_post_photo_video_color, facebook_post_photo_video_color_bk, (121, 177, 105)], True, group_name_mapping[nt.group_name_length])
-    mouse_click(group_name_mapping[nt.group_name_length]) # click the 'photo/video button'
+    if not open_photo_upload(nt):
+        clean_up()
     copy_paste(nt.folder_path, 'file_upload_path')
     sleep(1)
     press('backspace')
@@ -127,29 +134,36 @@ def run_group(nt): #google_doc_link, folder_path, group_tup, post_coords):
     press('enter')
     sleep(2) # takes some time to upload the photos
 
-    #press('ctrl', 'pagedown') # to google doc
-    mouse_click('tab_two')
-    wait_for_screen(drive_ready_color, True, 'google_drive_ready')
+    press('ctrl', 'pagedown') # to google doc
+    #mouse_click('tab_two')
+    if not wait_for_screen(drive_ready_color, True, 'google_drive_ready'):
+        clean_up()
     mouse_click('google_drive_ready')
     press('ctrl', 'a') # select the text
     sleep(1)
     press('ctrl', 'c') # copy the text
     sleep(1)
-    #press('ctrl', 'pageup') # back to facebook
-    mouse_click('tab_one')
+    press('ctrl', 'pageup') # back to facebook
+    #mouse_click('tab_one')
     sleep(1)
     mouse_click('write_something')
     press('ctrl', 'v')
-    wait_for_screen(facebook_ready_to_post_color, True, '', *nt.post_button_coords)
+    if not wait_for_screen(facebook_ready_to_post_color, True, '', *nt.post_button_coords):
+        clean_up()
     pag.click(*(nt.post_button_coords))
     sleep(5) # we still need to be able to wait for the post pending screen
 
-    wait_for_screen(facebook_post_pending_color, False, '', *nt.post_pending_coords)
+    if not wait_for_screen(facebook_post_pending_color, False, '', *nt.post_pending_coords):
+        clean_up()
     sleep(1)
-    mouse_click('close_chrome')
+    clean_up()
 
 if __name__=='__main__':
+    schools = ['SJSU', 'USC']
     for data in all_groups:
-        run_group(data)
+        if data.short_name in schools:
+            run_group(data)
+        else:
+            print('Skipping {}'.format(data.short_name))
 
 
