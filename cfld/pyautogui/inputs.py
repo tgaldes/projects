42d17f0868ba4f17392bb0af22bd1768ea1913ef
Tyler <tgaldes@gmail.com>
2020-09-11 01:01:15 -0400

    Support posting in buy/sell groups

--- cfld/pyautogui/inputs.py
+++ cfld/pyautogui/inputs.py
@@ -1,5 +1,9 @@
 from collections import namedtuple
-school_data = {'USC' :(
+
+
+
+# Normal housing groups
+group_data = {'USC' :(
              'https://docs.google.com/document/d/1wOmbPiLod6B-WPjF4ygP6JbjqGP8Em4VXPMk0yekX_8',
              '/home/tgaldes/Dropbox/Fraternity PM/Pictures/2715 Portland USC/selections',
              [
@@ -32,19 +36,70 @@ school_data = {'USC' :(
             '/home/tgaldes/Dropbox/Fraternity PM/Pictures/Minnesota Kappa Sigma/Facebook',
             [
                 ('https://www.facebook.com/groups/439773589790851/', 3),
-                ('https://www.facebook.com/groups/153997765269550/', 2)
+                ('https://www.facebook.com/groups/153997765269550/', 2),
+                ('https://www.facebook.com/groups/607733796013823/', 1)
             ],
              (1194, 1419), # coords to the 'post' button when we c/p the blurb into the text box
              (1113, 282)  # coords to the area that we check for post pending
          )
         }
+buy_sell_data = {'USC' :(
+             'https://docs.google.com/document/d/1wOmbPiLod6B-WPjF4ygP6JbjqGP8Em4VXPMk0yekX_8',
+             '/home/tgaldes/Dropbox/Fraternity PM/Pictures/2715 Portland USC/selections',
+             [
+                 #('https://www.facebook.com/groups/151027485336692/?ref=gysj', 1),
+                 #('https://www.facebook.com/groups/55449183852/', 2), 
+                 #('https://www.facebook.com/groups/roompiklosangelesroommaterentals', 2),
+                 ('https://www.facebook.com/groups/LosAngelesHousingRentals/', 2),
+                 ('https://www.facebook.com/groups/1286269061395308/', 2),
+                 ('https://www.facebook.com/groups/LA4stay/', 2),
+             ],
+             'USC Student Housing',
+             '550'
+        ),
+        'SJSU' :
+        (
+            'https://docs.google.com/document/d/1jxyqgdu0EXNK73gg5kcapxTcLc0XilB1qrkAbe31BtU',
+            '/home/tgaldes/Dropbox/Fraternity PM/Pictures/SJSU TX/facebook',
+            [
+                ('https://www.facebook.com/groups/1153022088056808/', 2),
+                ('https://www.facebook.com/groups/1426595240988253/', 3),
+                ('https://www.facebook.com/groups/2023562321211089', 3),
+                ('https://www.facebook.com/groups/I3027/', 2),
+                ('https://www.facebook.com/groups/bayareagradhousing2015', 2),
+            ],
+             'SJSU Student Housing',
+             '650',
+        ),
+        'UMN' :
+        (
+            'https://docs.google.com/document/d/17Kx_8SGCm3QA-ArL8o7GU99cOyqjLJX02lJogHlAvKg',
+            '/home/tgaldes/Dropbox/Fraternity PM/Pictures/Minnesota Kappa Sigma/Facebook',
+            [
+                ('https://www.facebook.com/groups/Minneapolisrentals/', 3),
+                ('https://www.facebook.com/groups/TwinCityRentals/', 3),
+            ],
+            'UMN Student Housing',
+            '750',
+         ),
+        }
 
 GroupData = namedtuple('GroupData', ['google_drive_link', 'folder_path', 'group_link', 'group_name_length', 'post_button_coords', 'post_pending_coords', 'short_name'])
+BuySellData = namedtuple('BuySellData', ['google_drive_link', 'folder_path', 'group_link', 'group_name_length', 'short_name', 'title', 'price'])
 
 all_groups = []
-for short_name in school_data:
-    for group in school_data[short_name][2]:
-        school = school_data[short_name]
+for short_name in group_data:
+    for group in group_data[short_name][2]:
+        school = group_data[short_name]
         data = GroupData(school[0], school[1], group[0], group[1], school[3], school[4], short_name)
         all_groups.append(data)
+
+all_buy_sells = []
+for short_name in buy_sell_data:
+    for group in buy_sell_data[short_name][2]:
+        school = buy_sell_data[short_name]
+        data = BuySellData(school[0], school[1], group[0], group[1], short_name, school[3], school[4])
+        all_buy_sells.append(data)
+
+
 
--- cfld/pyautogui/main.py
+++ cfld/pyautogui/main.py
@@ -1,50 +1,75 @@
+import pdb
 import pyautogui as pag
 pag.PAUSE = 1
 pag.FAILSAFE = True
 from time import sleep
 import os
 
-from inputs import all_groups
+from inputs import all_groups, all_buy_sells
 
 
 # hold corrdinates of various clicks we need in a map so that we can run indep of display
 group_name_mapping = {1 : 'one_line_group_photo_video', 
                       2 : 'two_line_group_photo_video',
                       3 : 'three_line_group_photo_video'}
+buy_sell_name_mapping = {1 : 'one_line_sell_something', 
+                      2 : 'two_line_sell_something',
+                      3 : 'three_line_sell_something'}
                       
 all_coords = {
     'l' : # laptop 
         { 'menu' : (100, 100) },
     'm' : # monitor
         { 
+# ubuntu
             'menu' : (23, 65),
             'chrome' : (168, 170),
+# chrome
             'close_chrome' : (91, 46),
             'url_bar' : (562, 78),
-            'one_line_group_photo_video' : (1066, 786),
-            'two_line_group_photo_video' : (1066, 820),
-            'file_upload_path' : (1202, 250),
-            'write_something' : (1144, 590),
-            'three_line_group_photo_video' : (1066, 853),
             'tab_one' : (264, 60),
             'tab_two' : (540, 60),
+            'open_files_bar' : (800, 225),
+# normal group
+            'file_upload_path' : (1202, 250),
+            'write_something' : (1144, 590),
+            'one_line_group_photo_video' : (1066, 786),
+            'two_line_group_photo_video' : (1066, 820),
+            'three_line_group_photo_video' : (1066, 853),
+# buy and sell group
+            'one_line_sell_something' : (1238, 737),
+            'two_line_sell_something' : (1238, 768),
+            'three_line_sell_something' : (1238, 810),
+            'add_photos' : (1111, 432),
+            'title' : (1121, 685),
+            'price' : (1121, 764),
+            'description' : (1121, 836),
+            'gray_next_that_shows_window_is_open' : (1111, 1285),
+            'blue_next' : (1169, 1486),
+# drive
             'google_drive_ready' : (949, 286),
-            'open_files_bar' : (800, 225)
         }
     }
 mkey = 'm' # TODO
 coords = all_coords[mkey]
 
+# TODO: redshift for non gray pixels (might not be needed)
+
+# refactor one: one function running a while loop waiting on pixels scrolling clicking etc
+# refactor two: move utils / group / buy sell funcs to their own files
 # gui constants
-# TODO: redshift for non gray pixels
 facebook_post_pending_color = (246, 246, 246)
 facebook_post_photo_video_color = (122, 183, 103) # green in the button
 facebook_post_photo_video_color_bk = (116, 173, 98) # green in the button
-#facebook_ready_to_post_color = (69, 117, 225) # white
 facebook_ready_to_post_color = (72, 123, 237) # blue
+facebook_ready_to_post_alt_color = (69, 117, 225) # blue
 drive_ready_color = (255, 255, 255)
 chrome_url_bar = (255, 255, 255)
 open_file = (71, 70, 64)
+buy_sell_post_pending_color = (229, 230, 235)
+buy_sell_window_open_color = (229, 230, 235)
+sell_something_button_mouseover = (217, 218, 223)
+
 
 def mouse_click(item, sleep_secs=0):
     pag.click(*coords[item])
@@ -61,6 +86,7 @@ def copy_paste(s, dest, fn='tmp_input.txt'):
     with open(fn, 'w') as f:
         f.write(s)
         f.flush()
+    sleep(.1)
     # copy
     os.system('cat {} | xclip -selection clipboard'.format(fn))
     # select dest
@@ -109,7 +135,126 @@ def open_photo_upload(nt):
         if count > 120:
             return False
 
+def open_buy_sell_photo_upload(nt):
+    count = 0
+    while True:
+        screen = pag.screenshot()
+        mouse_click('add_photos')
+        sleep(1)
+        if pag.pixelMatchesColor(*coords['open_files_bar'], open_file):
+            return True
+        count += 1
+        print('pixel {} coords {} matching {} actual {}'.format(coords['open_files_bar'], open_file, True, screen.getpixel(coords['open_files_bar'])))
+        if count > 120:
+            return False
 
+def wait_for_buy_sell_to_load(nt):
+    count = 0
+    while True:
+        # scroll up
+        pag.scroll(1000)
+        screen = pag.screenshot()
+        # wait to see if the grayed out 'Next' button appears
+        if pag.pixelMatchesColor(*coords['gray_next_that_shows_window_is_open'], buy_sell_window_open_color):
+            mouse_click(buy_sell_name_mapping[nt.group_name_length])
+            return True
+        print('pixel {} coords {} matching {} actual {}'.format(coords['gray_next_that_shows_window_is_open'], buy_sell_window_open_color, True, screen.getpixel(coords['gray_next_that_shows_window_is_open'])))
+        count += 1
+        if count > 120:
+            return False
+
+def open_buy_sell_window(nt):
+    count = 0
+    sleep(2)
+    while True:
+        move_to(buy_sell_name_mapping[nt.group_name_length])
+        # we need to move the mouse off the button in case the page loads with the cursor already on the screen
+        pag.moveRel(200, 200, .1) # move down to the right for .1 seconds
+        pag.moveRel(-200, -200, .1) # move back
+        # scroll up
+        pag.scroll(1000)
+        screen = pag.screenshot()
+        # wait to see if the grayed out 'Next' button appears
+        if pag.pixelMatchesColor(*coords[buy_sell_name_mapping[nt.group_name_length]], sell_something_button_mouseover):
+            mouse_click(buy_sell_name_mapping[nt.group_name_length])
+            return wait_for_buy_sell_to_load(nt)
+        print('pixel {} coords {} matching {} actual {}'.format(coords[buy_sell_name_mapping[nt.group_name_length]], sell_something_button_mouseover, True, screen.getpixel(coords[buy_sell_name_mapping[nt.group_name_length]])))
+        count += 1
+        if count > 120:
+            return False
+
+
+def buy_sell_wait_for_photo_upload(nt):
+    count = 0
+    while True:
+        # scroll up
+        pag.scroll(-1000)
+        screen = pag.screenshot()
+        # wait until it turns blue
+        if pag.pixelMatchesColor(*coords['blue_next'], facebook_ready_to_post_color):
+            return True
+        if pag.pixelMatchesColor(*coords['blue_next'], facebook_ready_to_post_alt_color):
+            return True
+        print('pixel {} coords {} matching {} actual {}'.format(coords['blue_next'], [facebook_ready_to_post_alt_color, facebook_ready_to_post_color], True, screen.getpixel(coords['blue_next'])))
+        count += 1
+        if count > 120:
+            return False
+def run_buy_sell_group(nt):
+    print(nt)
+    mouse_click('menu')
+    keyboard('Chrome')
+    mouse_click('chrome')
+    if not wait_for_screen(chrome_url_bar, True, 'url_bar'):
+        clean_up()
+        return
+    open_tab(nt.group_link)
+    press('ctrl', 't')
+    open_tab(nt.google_drive_link)
+    press('ctrl', 'pageup') # back to facebook tab
+    if not open_buy_sell_window(nt):
+        clean_up()
+        return
+    # Populate the title, price from the named tuple
+    copy_paste(nt.title, 'title')
+    copy_paste(nt.price, 'price')
+    press('ctrl', 'pagedown') # to google doc
+    #mouse_click('tab_two')
+    if not wait_for_screen(drive_ready_color, True, 'google_drive_ready'):
+        clean_up()
+        return
+    mouse_click('google_drive_ready')
+    press('ctrl', 'a') # select the text
+    sleep(1)
+    press('ctrl', 'c') # copy the text
+    sleep(1)
+    press('ctrl', 'pageup') # back to facebook
+    #mouse_click('tab_one')
+    sleep(1)
+    mouse_click('description')
+    press('ctrl', 'v')
+
+    # upload the photos
+    if not open_buy_sell_photo_upload(nt):
+        clean_up()
+        return
+    copy_paste(nt.folder_path, 'file_upload_path')
+    sleep(1)
+    press('backspace')
+    press('enter')
+    sleep(1)
+    # select all photos and go
+    press('ctrl', 'a')
+    press('enter')
+    sleep(2) # takes some time to upload the photos
+
+    # now we'll scroll down to the bottom of the sc
+    buy_sell_wait_for_photo_upload(nt)
+    mouse_click('blue_next')
+# same func to scroll down past the list of other buy sell groups
+    buy_sell_wait_for_photo_upload(nt)
+    mouse_click('blue_next')
+    sleep(5)
+    clean_up()
 
 #def run_group(google_doc_link, folder_path, group_tup, post_coords):
 def run_group(nt): #google_doc_link, folder_path, group_tup, post_coords):
@@ -118,12 +263,14 @@ def run_group(nt): #google_doc_link, folder_path, group_tup, post_coords):
     mouse_click('chrome')
     if not wait_for_screen(chrome_url_bar, True, 'url_bar'):
         clean_up()
+        return
     open_tab(nt.group_link)
     press('ctrl', 't')
     open_tab(nt.google_drive_link)
     press('ctrl', 'pageup') # back to facebook tab
     if not open_photo_upload(nt):
         clean_up()
+        return
     copy_paste(nt.folder_path, 'file_upload_path')
     sleep(1)
     press('backspace')
@@ -138,6 +285,7 @@ def run_group(nt): #google_doc_link, folder_path, group_tup, post_coords):
     #mouse_click('tab_two')
     if not wait_for_screen(drive_ready_color, True, 'google_drive_ready'):
         clean_up()
+        return
     mouse_click('google_drive_ready')
     press('ctrl', 'a') # select the text
     sleep(1)
@@ -150,20 +298,34 @@ def run_group(nt): #google_doc_link, folder_path, group_tup, post_coords):
     press('ctrl', 'v')
     if not wait_for_screen(facebook_ready_to_post_color, True, '', *nt.post_button_coords):
         clean_up()
+        return
     pag.click(*(nt.post_button_coords))
     sleep(5) # we still need to be able to wait for the post pending screen
 
     if not wait_for_screen(facebook_post_pending_color, False, '', *nt.post_pending_coords):
         clean_up()
+        return
     sleep(1)
     clean_up()
 
 if __name__=='__main__':
-    schools = ['SJSU', 'USC']
-    for data in all_groups:
-        if data.short_name in schools:
-            run_group(data)
-        else:
-            print('Skipping {}'.format(data.short_name))
+    schools = ['SJSU']
+    selections = ['buy_sell']
+    if 'group' in selections:
+        for data in all_groups:
+            if data.short_name in schools:
+                run_group(data)
+            else:
+                print('Skipping {}'.format(data.short_name))
+    else:
+        print('Skipping group')
 
 
+    if 'buy_sell' in selections:
+        for data in all_buy_sells:
+            if data.short_name in schools:
+                run_buy_sell_group(data)
+            else:
+                print('Skipping {}'.format(data.short_name))
+    else:
+        print('Skipping buy_sell')
