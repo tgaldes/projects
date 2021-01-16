2d8f81b7c11348492a7e2bb670933abb73db1305
Tyler <tgaldes@gmail.com>
2021-01-15 22:11:21 -0500

    Extract actions that use a destination to a base class.

---
+++ cfld/pyautogui/scripts/run_buildium.sh
@@ -0,0 +1,3 @@
+export PYTHONPATH=/cfld/projects/cfld/pyautogui
+/usr/bin/python3 /cfld/projects/cfld/pyautogui/buildium/main.py "$@"
+
---
+++ cfld/pyautogui/scripts/run_facebook.sh
@@ -0,0 +1,2 @@
+export PYTHONPATH=/cfld/projects/cfld/pyautogui
+/usr/bin/python3 /cfld/projects/cfld/pyautogui/facebook/main.py "$@"
