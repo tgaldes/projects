ef010db45910bd35577bdbe3c5020be7ea78c9a0
Tyler <tgaldes@gmail.com>
2021-09-21 22:36:27 -0400

    add pip install module commands

---
+++ cfld/gmail/deploy/pip_install_commands.sh
@@ -0,0 +1,7 @@
+pip3 install flatten_dict
+pip3 install google-api-python-client
+pip3 install google_auth_oauthlib
+pip3 install interface
+pip3 install Interface
+pip3 install python-interface
+pip3 install logging_tree
